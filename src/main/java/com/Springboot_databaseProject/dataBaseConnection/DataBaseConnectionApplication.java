4ed4377b2746333d568ab4f807036fe7e10d51c3
siddHacks <siddtheprofessional@gmail.com>
2023-04-28 11:17:05 +0530

    Initial commit

---
+++ src/main/java/com/Springboot_databaseProject/dataBaseConnection/DataBaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot_databaseProject.dataBaseConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Springboot_databaseProject/dataBaseConnection/DataBaseConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot_databaseProject.dataBaseConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
